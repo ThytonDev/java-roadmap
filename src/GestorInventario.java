19f88fa037b9c2eb127cbe5842785026e196c204
ThytonDev <thytonverse@gmail.com>
2026-09-08 14:30:59 +0200

    primera parte de la leccion final de la fase 0

--- src/GestorInventario.java
+++ src/GestorInventario.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 public class GestorInventario {
     public static void main() {
-
+        //Crear nuevo inventario
+        Inventario inventario = new Inventario();
+        //Añadir los productos
+        inventario.agregar(new Producto("Fresno", 3.0, 30));
+        inventario.agregar(new Producto("Castaño", 2.0, 50));
+        inventario.agregar(new Producto("Arce", 5.0, 25));
+        inventario.agregar(new Producto("Roble", 8.0, 80));
+        inventario.agregar(new Producto("Bambu", 1.5, 95));
+        //Listar inventario
+        inventario.listar();
     }
 }
